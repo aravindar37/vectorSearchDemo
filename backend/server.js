7ff8cfc09936f0f77698852209f99af9bc9869f5
Aravind <aravindar37@gmail.com>
2025-01-14 21:57:43 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const { MongoClient, ObjectId } = require("mongodb");
+const cors = require("cors");
+const WebSocket = require("ws");
+const http = require("http");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const chatAgent = require("./services/participantResponse");
+const participantResponse = require("./services/participantResponse");
+const processAndStoreStory = require("./services/storyService");
+const movieSearch = require("./services/movieSearch");
+const { OAuth2Client } = require('google-auth-library');
+const gClient = new OAuth2Client("701927321552-fsujubeohfqfp8mpm27k431v334m93l3.apps.googleusercontent.com");
+
+const app = express();
+const server = http.createServer(app);
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const url = "mongodb+srv://username:password@server.project.mongodb.net/"; //Atlas URL
+const dbName = "vectorDB";
+let db;
+
+const client = new MongoClient(url);
+db = client.db(dbName);
+
+// API endpoint for movie search
+app.get("/api/movies", async (req, res) => {
+  try {
+    const database = client.db("sample_mflix");
+    console.log("Fetching random movies");
+    const movies = await database.collection("searchMovies").aggregate([
+      {
+        $sample: {
+          size: 10,
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          title: 1,
+          imageUrl: "$poster",
+        },
+      },
+    ]).toArray();
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.get("/api/movies/search", async (req, res) => {
+    try {
+      const searchString = req.query.key;
+      const decodedString = decodeURIComponent(searchString);
+      const database = client.db("sample_mflix");
+      console.log(`Searching for movies with: ${decodedString}`);
+      const movies = await movieSearch(decodedString, database);
+      res.status(200).json(movies);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+
+const PORT = process.env.PORT || 5002;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
