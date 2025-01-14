d8880befb757257b290f519f7f3c2cc0cdf3d5e0
Aravind <aravindar37@gmail.com>
2025-01-14 22:03:34 +0530

    Create package.js

---
+++ backend/package.js
@@ -0,0 +1,34 @@
+{
+  "name": "chat-be",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "description": "",
+  "dependencies": {
+    "@langchain/core": "^0.3.22",
+    "@langchain/mongodb": "^0.1.0",
+    "@langchain/ollama": "^0.1.2",
+    "@langchain/textsplitters": "0.1.0",
+    "axios": "^1.7.9",
+    "bcrypt": "5.1.1",
+    "bson": "^6.10.1",
+    "cors": "2.8.5",
+    "dotenv": "16.4.7",
+    "event-stream": "^4.0.1",
+    "express": "^4.21.1",
+    "mongodb": "^6.12.0",
+    "winston": "^3.17.0",
+    "ws": "^8.18.0"
+  },
+  "resolutions": {
+    "@langchain/core": "^0.3.22"
+  },
+  "overrides": {
+    "@langchain/core": "^0.3.22"
+  }
+}
